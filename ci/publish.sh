26a00716b93326dd41a836e0ce33f7ff9926b418
Tobias Gerschner <tobias.gerschner@gmail.com>
2017-04-29 12:05:31 +1200

    basic circle ci config

--- ci/publish.sh
+++ ci/publish.sh
@@ -12,6 +12,7 @@ cat <<-EOT
     -h or --help : Prints this help
     -p or --prod : Deploy to production
     -d or --debug: Debug script
+    -f or --force: Force publishing
 
 EOT
 
@@ -19,9 +20,10 @@ EOT
 
 run_update() {
 
-    aws-profile --switch sapient
+    [ -n "$CI" ] || aws-profile --switch sapient
     aws s3 sync "$SCRIPT_SOURCE_DIR/public" "s3://$PUBLIC_BUCKET"
-    git commit -m "AS PUBLISHED @ `date +F-%H-%M`" public
+    git add public
+    git commit -m "AS PUBLISHED @ `date +%F-%H-%M`" public
     git push
 
 }
@@ -31,17 +33,20 @@ export SCRIPT_SOURCE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 
 [ "`type source`" == "source is a shell builtin" ] || abort_on_error "This script is written for Bash!"
 
-OPTS=`getopt -o b:hd --long bucket:,help,debug -n 'parse-options' -- "$@"`
+OPTS=`getopt -o b:hdf --long bucket:,help,debug,force -n 'parse-options' -- "$@"`
 
 [ $? == 0 ] || abort_on_error "Failed parsing options."
 
 eval set -- $OPTS
+
+FORCE=false
 
 while true; do
   case "$1" in
     -p | --prod ) PROD="true";;
     -h | --help ) print_help ;;
     -d | --debug ) DEBUG=true ;;
+    -f | --force ) FORCE=true ;;
     -- ) REMAINING_ARGS="$@" ; break ;;
   esac
   shift
@@ -51,15 +56,16 @@ done
 
 if [ "$PROD" == "true" ]
 then
-    PUBLIC_BUCKET="kapiti-digital-youth-challenge-2017"
+    PUBLIC_BUCKET="youth-challenge.kapiti.co"
 else
-    PUBLIC_BUCKET="kapiti-digital-youth-challenge-2017.kapiti.co"
+    PUBLIC_BUCKET="preview-yc-2017.kapiti.co"
 fi
 
 pushd $SCRIPT_SOURCE_DIR > /dev/null 2>&1
 
     hugo
     git status --porcelain |grep -q 'M public' ; changed=$?
+    [ "$FORCE" == "true" ] ; changed=0
     if [ "$changed" == "0" ]
     then
         echo "Publishing changes"
